6f2fcc129a88773eda685b215ae4dd38439edf65
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-12 01:21:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.shift(element);
+  return array;
+
+}
