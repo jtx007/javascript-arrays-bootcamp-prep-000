b7352566cd77a011881755a06229fd01a9a7acdb
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-12 01:28:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
- var newArray = array.slice(0);
+ var newArray = array.slice(1);
  return newArray;
 }
