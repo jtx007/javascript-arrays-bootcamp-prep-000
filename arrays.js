abefa56f268cc2b7b1602d6c34dba6fa1869b8a8
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-12 01:18:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,8 @@ function addElementToBeginningOfArray(array, element) {
 return [element,...array]
 
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
